7fcc4dccf6c33f66f256534a0811b58365558572
Heru Apriadi <heru@investree.id>
2021-12-30 02:07:22 +0800

    try to add xk6 extension

--- k6_example.js
+++ k6_example.js
@@ -1,7 +1,9 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import faker from "k6/x/faker"
 
 export default function () {
   http.get('https://blazedemo.com/');
   sleep(1);
+  console.log('TEST FAKER XK6 random name: '+faker.name())
 }
